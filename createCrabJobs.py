1a01a304f717399f9beea295f598a2655b1b7765
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2013-08-15 18:18:46 +0200

    first version

--- createCrabJobs.py
+++ createCrabJobs.py
@@ -30,23 +30,36 @@ for label in LHEs.keys():
   subPro.wait()
   print(subPro.communicate()[0])
   lheCrab[label]["crabDir"] = crabDir
-  lheCrab[label]["pset"] = crabDir+os.path.sep+commandArgPostfix("--python_filename",label)
+  lheCrab[label]["crabCfg"] = {}; crabCfg = lheCrab[label]["crabCfg"]
+  crabCfg["CMSSW"] = {}
+  crabCfg["CMSSW"]["pset"] = crabDir+os.path.sep+commandArgPostfix("--python_filename",label)
   lheCrab[label]["output_file"] = commandArgPostfix("--python_filename",label)
-  lheCrab[label]["user_remote_dir"] = mainFolder.rstrip('/')+os.path.sep+ LHEs[label]["gridFolderName"]+"_"+label+"_"+timeSt
-  lheCrab[label]["additional_input_files"]=  LHEs[label]["pathToLhes"].rstrip('/')+os.path.sep+label+".lhef"
-  lheCrab[label]["publish_data_name"] =  LHEs[label]["publishName"]+"_"+label
+  crabCfg["USER"]={}
+  crabCfg["USER"]["user_remote_dir"] = mainFolder.rstrip('/')+os.path.sep+ LHEs[label]["gridFolderName"]+"_"+label+"_"+timeSt
+  crabCfg["USER"]["additional_input_files"] =  LHEs[label]["pathToLhes"].rstrip('/')+os.path.sep+label+".lhef"
+  crabCfg["USER"]["publish_data_name"] =  LHEs[label]["publishName"]+"_"+label
+  crabCfg["CMSSW"]["generator"]="lhe"
+  crabCfg["CMSSW"]["number_of_jobs"]=1
+  crabCfg["CMSSW"]["total_number_of_events"]=100000000
+  crabCfg["USER"]["publish_data"]=1
+  crabCfg["USER"]["dbs_url_for_publication"]="https://cmsdbsprod.cern.ch:8443/cms_dbs_ph_analysis_02_writer/servlet/DBSServlet"
 ##########
-##for l,cJ in lheCrab.iteritems():
-##    crabJob = CrabTools.crabProcess(l,cJ["pset"],"none",cJ["crabDir"],addGridDir="")
-##    crabP.createCrabCfg()
-##    if sampDict.has_key("crabConfig"):
+for l,cJ in lheCrab.iteritems():
+    crabJob = CrabTools.crabProcess(l,"none","none",cJ["crabDir"],timeSt,mainFolder)
+    crabJob.setCrabDir()
+    crabJob.createCrabDir()
+    crabCfg = crabJob.createCrabCfg(cJ["crabCfg"])
+    print crabCfg    
+ 
+#    crabP.createCrabCfg()
+#    if sampDict.has_key("crabConfig"):
 ##      for k1 in sampDict["crabConfig"].keys():
 ##        for k2 in sampDict["crabConfig"][k1].keys():
 ##          crabP.crabCfg[k1][k2]=sampDict["crabConfig"][k1][k2]
 ##    crabP.crabCfg["CMSSW"]["total_number_of_events"]=1000000
 ##    crabP.crabCfg["CMSSW"]["number_of_jobs"]= 100
 ##    crabCfgFilename = crabP.createCrabDir()
-##    crabP.writeCrabCfg()
-##    crabP.executeCrabCommand("-create",debug = True)
+    crabJob.writeCrabCfg()
+    crabJob.executeCrabCommand("-create",debug = True)
 ##    CrabTools.saveCrabProp(crabP,options["outputPath"]+"/"+postfix+"_"+timeStamp+"_CrabCfg.json")
 ##    crabP.executeCrabCommand("-submit",debug = True)
